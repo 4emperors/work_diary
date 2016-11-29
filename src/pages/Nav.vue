f62869ef9232e7359bafbac6e5b7edc7bf5e66d0
付维翔 <fuwx@qianfan365.com>
2016-11-29 11:51:38 +0800

    添加班统计链接

--- src/pages/Nav.vue
+++ src/pages/Nav.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="bottom_nav">
         <ul class="nav_ul">
-            <li><router-link :to="'home'">加班</router-link></li>
+            <li><router-link :to="'home'">首页</router-link></li>
+            <li><router-link :to="'overtime'">加班</router-link></li>
             <li><router-link :to="'todo'">备忘</router-link></li>
             <li>我</li>
          </ul>
@@ -20,7 +21,7 @@
      .bottom_nav li{
        height:45px;
        line-height:45px;
-        width:30%;
+        width:20%;
         display: inline-block;
      }
 </style>
